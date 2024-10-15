272ccf15fc2031648dbec6ccbd5cf45e57c29aff
qwerasdfxzdcv <cshxx992@gmail.com>
2024-10-15 14:14:15 +0900

    UserRepositoryTest 테스트케이스 구현

--- src/main/java/com/example/jpa_test/repository/UserRepository.java
+++ src/main/java/com/example/jpa_test/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.jpa_test.repository;
 import com.example.jpa_test.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository
         extends JpaRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
 }
